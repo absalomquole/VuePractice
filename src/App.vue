3ffd18d33626b59ee058d77715c35e99a29af7e9
absalomquole <insanesanky@gmail.com>
2018-04-06 16:32:41 +0530

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <img width="25%" src="./assets/logo.png">
+    
+    <NewComp />
+  </div>
+</template>
+
+<script>
+import HelloWorld from "./components/HelloWorld";
+import NewComp from "./components/NewComp";
+export default {
+  name: "App",
+  components: {
+    HelloWorld,
+    NewComp
+  }
+};
+</script>
+
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
